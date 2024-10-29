e84711a305b896d15048f78e2cea23dd1c836522
Elo-oghene Olokor <81395502+Elo-oghene@users.noreply.github.com>
2024-10-29 13:36:34 +0100

    added footer

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<template>
+    <div class="w-full h-auto px-4 md:px-8 lg:px-16 py-4 border-t-2 border-t-[#CEDD71] bg-black flex flex-col md:flex-row justify-between items-center gap-4 text-center md:text-left">
+    <div class="flex items-center gap-2">
+        <img class="w-12 lg:w-16 object-contain" src="../../assets/images/GR GENERAL LOGO 1.png" alt="logo" />
+        <p class="text-[#CEDD71] font-bold text-xs md:text-sm lg:font-semibold">Graceville International<br/> Schools</p>
+    </div>
+    <div class="text-[#CEDD71] text-xs md:text-sm lg:text-base">
+        Copyright © 2022 Graceville International School, Asaba. All rights reserved.
+    </div>
+</div>
+
+</template>
--- src/components/landing/Home.vue
+++ src/components/landing/Home.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Layout from './Layout.vue';
+import Footer from '../footer/Footer.vue'
 
 defineOptions({
     layout: Layout
+    
 });
 </script>
 
@@ -15,11 +17,17 @@ defineOptions({
             <img src="../../assets/images/academic-block.jpg" class="w-full h-full bg-cover" alt="Building Image">
             <div class="absolute inset-0 bg-black opacity-50"></div>
         </div>
-        <div class="absolute top-44 lg:top-52 left-4 lg:left-10 flex flex-col gap-2 z-10">
-            <p class="text-lg text-white tracking-widest">WELCOME TO OUR OFFICIAL WEBSITE</p>
-            <h1 class="text-[30px] lg:text-[100px] leading-[40px] lg:leading-[105px] text-white font-cormorant">Graceville <br> International School</h1>
-            <p class="text-white text-sm md:text-sm font-serif">Thanks so much for visiting, we are delighted that you are interested in our schools and also at enrolling your child.</p>
-            <button class="self-start px-4 py-3 bg-[#CEDD71] text-sm rounded-sm shadow-md hover:bg-[#b0cc63] hover:scale-105 hover:shadow-lg transition-all duration-300 ease-in-out">APPLY FOR ADMISSION</button>
+        <div class="absolute top-44 lg:top-52 left-4 lg:left-10 flex flex-col pt-10 gap-2 lg:gap-4 z-10">
+            <p class="text-xs lg:text-lg text-white tracking-widest">WELCOME TO OUR OFFICIAL WEBSITE</p>
+            <div class="text-[30px] leading-none lg:text-[80px] text-white font-cormorant"> 
+                <h1>Graceville</h1>
+                <h1>Group Of School</h1>
+            </div>
+            <p class="text-white text-xs lg:text-sm md:text-sm font-serif tracking-wider">Thanks so much for visiting, we are delighted that you are interested in our schools and also at enrolling your child.</p>
+            <div class="flex flex-col lg:flex-row gap-4 pt-4 lg:gap-16">
+                <button class="self-start px-4 py-3 bg-[#CEDD71] text-sm rounded-sm shadow-md hover:bg-[#b0cc63] hover:scale-105 hover:shadow-lg transition-all duration-300 ease-in-out">APPLY FOR ADMISSION</button>
+                <button class="self-start px-4 py-3 bg-[#CEDD71] text-sm rounded-sm shadow-md hover:bg-[#b0cc63] hover:scale-105 hover:shadow-lg transition-all duration-300 ease-in-out">PARENT LOGIN</button>
+            </div>
         </div>
 
         <section class="w-full flex flex-col lg:flex-row items-center justify-center bg-black pt-10 pb-4">
@@ -165,13 +173,13 @@ defineOptions({
         </section>
         
         <section class="w-full flex flex-col lg:flex-row bg-black gap-2 px-4">
-            <div class="w-full lg:w-1/2 flex flex-col items-center  pt-6">
+            <div class="w-full lg:w-1/2 flex flex-col items-center pt-6 gap-2">
                 <div class="w-full">
                         <img src="../../assets/images/cultural.png" class="w-full" alt="">
                     </div>
                 <div class="text-[#CEDD71] text-[20px] lg:text-[20px]">Cultural Activities</div>
             </div>
-            <div class="w-full lg:w-1/2 flex flex-col items-center pt-6 gap-2">
+            <div class="w-full lg:w-1/2 flex flex-col items-center pt-6 pb-4 gap-2">
                 <div class="w-full">
                         <img src="../../assets/images/sport2.png" class="w-full" alt="">
                     </div>
@@ -181,7 +189,7 @@ defineOptions({
 
         <section class="w-full flex flex-col items-center h-full bg-[#224E05] pt-10 px-4 justify-center">
             <div class="w-full lg:w-[80%] flex flex-col lg:flex-row">
-                <div class="w-full lg:w-1/4 bg-[#224E05] flex flex-col justify-center items-center">
+                <div class="w-full lg:w-1/4 bg-[#224E05]  flex flex-col justify-center items-center">
                     <div class="text-[#CEDD71] tracking-widest">VIEW SOME OF</div>
                     <div class="text-white font-cormorant text-[50px]">Our Facilities</div>
                 </div>
@@ -189,53 +197,109 @@ defineOptions({
                     <div class="w-full h-full">
                         <img src="../../assets/images/cot.jpg" class="w-full h-full object-cover" alt="">
                     </div>
-                    <div class="text-[#CEDD71] text-[20px] lg:text-[20px] tracking-widest">SPORTS CENTRE</div>
+                    <div class="text-[#CEDD71] text-[16px] lg:text-[20px] tracking-widest">SPORTS CENTRE</div>
                 </div>
                 <div class="w-full lg:w-1/4  flex flex-col items-center lg:h-[350px] gap-2">
                     <div class="w-full h-full">
                         <img src="../../assets/images/lab.jpg" class="w-full h-full object-cover" alt="">
                     </div>
-                    <div class="text-[#CEDD71] text-[20px] lg:text-[20px] tracking-widest">PHYSICS LAB</div>
+                    <div class="text-[#CEDD71] text-[16px] lg:text-[20px] tracking-widest">PHYSICS LAB</div>
                 </div>
             </div>
-            <div class="w-full lg:w-[80%] flex flex-col lg:flex-row gap-4 mt-10">
+            <div class="w-full lg:w-[80%] flex flex-col lg:flex-row gap-4 lg:mt-10">
                 <div class="w-full lg:w-1/2 h-full flex flex-col items-center gap-2">
                     <div class="w-full h-full">
                         <img src="../../assets/images/pool.png" class="w-full h-full object-cover" alt="">
                     </div>
-                    <div class="text-[#CEDD71] text-[20px] lg:text-[20px] tracking-widest">A SWIMMING POOL</div>
+                    <div class="text-[#CEDD71] text-[16px] lg:text-[20px] tracking-widest">A SWIMMING POOL</div>
                 </div>
                 <div class="w-full lg:w-1/2 h-full flex flex-col items-center gap-2">
                     <div class="w-full h-full">
                         <img src="../../assets/images/lab1.png" class="w-full h-full object-cover" alt="">
                     </div>
-                    <div class="text-[#CEDD71] text-[20px] lg:text-[20px] tracking-widest">BIOLOGY LAB</div>
+                    <div class="text-[#CEDD71] text-[16px] lg:text-[20px] tracking-widest">BIOLOGY LAB</div>
                 </div>
-                <div class="w-full lg:w-1/2 h-full">
+                <div class="w-full lg:w-1/2 h-full flex flex-col items-center gap-2">
                     <div class="w-full h-full">
                         <img src="../../assets/images/ict-lab.jpg" class="w-full h-full object-cover" alt="">
                     </div>
-                    <div class="text-[#CEDD71] text-[20px] lg:text-[20px] text-center tracking-widest">ICT LAB</div>
+                    <div class="text-[#CEDD71] text-[16px] lg:text-[20px] text-center tracking-widest">ICT LAB</div>
                 </div>
-                <div class="w-full lg:w-1/2 h-full">
+                <div class="w-full lg:w-1/2 h-full flex flex-col items-center gap-2 pb-4">
                     <div class="w-full h-full">
                         <img src="../../assets/images/chem-lab.jpg" class="w-full h-full object-cover" alt="">
                     </div>
-                    <div class="text-[#CEDD71] text-[20px] lg:text-[20px] text-center tracking-widest">CHEMISTRY LAB</div>
+                    <div class="text-[#CEDD71] text-[16px] lg:text-[20px] text-center tracking-widest">CHEMISTRY LAB</div>
                 </div>
             </div>
         </section>
 
-        <section class="w-full h-[700px] bg-gray-500 bg-cover relative bg-[url('/src/assets/images/library.jpg')]">
+        <section class="w-full h-[700px] bg-cover bg-fixed relative bg-[url('/src/assets/images/library.jpg')]">
             <div class="absolute inset-0 bg-[rgba(0,0,0,0.7)] flex flex-col gap-4 items-center justify-center">
                 <p class="text-white text-xs lg:text-lg tracking-widest">INTERESTED IN JOINING US?</p>
                 <div class="text-center leading-none">
-                    <h1 class="text-white text-[60px] font-cormorant">Join Us For Information About New</h1>
-                    <h1 class="text-white text-[60px] font-cormorant">Student Admissions</h1>
+                    <h1 class="text-white text-[30px] lg:text-[60px] font-cormorant">Join Us For Information About New</h1>
+                    <h1 class="text-white text-[30px] lg:text-[60px] font-cormorant">Student Admissions</h1>
                 </div>
-                <p class="text-white tracking-wider">We are currently admitting new students into all our schools, click the button below to apply now.</p>
+                <p class="text-white tracking-wider text-xs text-center">We are currently admitting new students into all our schools, click the button below to apply now.</p>
                 <button class="px-4 py-3 bg-black text-sm text-white rounded-sm shadow-md tracking-wider hover:scale-105 hover:shadow-lg transition-all duration-300 ease-in-out">CLICK HERE</button>
             </div>
+        </section>
+
+        <section class="w-full bg-[#224E05] flex flex-col gap-4 pt-10 pb-10 px-6">
+            <div class="text-[24px] lg:text-[40px] text-white font-cormorant text-center">Visit Our Schools</div>
+            <div class="w-full flex flex-wrap gap-6 justify-center">
+                <div class="w-[250px] h-[250px] rounded-lg py-4 border-2 border-white flex justify-between items-center flex-col text-white transition-transform transition-shadow duration-300 ease-in-out hover:scale-105 hover:shadow-xl hover:border-[#CEDD71]">
+                    <div class="text-center">
+                        <p>Graceville International</p>
+                        <p>Schools</p>
+                    </div>
+                    <p>Off Ken Mozia Road,</p>
+                    <p>Asaba, Delta State</p>
+                    <p>0915 879 0633</p>
+                </div>
+                <div class="w-[250px] h-[250px] rounded-lg py-4 border-2 border-white flex justify-between items-center flex-col text-white transition-transform transition-shadow duration-300 ease-in-out hover:scale-105 hover:shadow-xl hover:border-[#CEDD71]">
+                    <div class="text-center">
+                        <p>Graceville International</p>
+                        <p>Schools</p>
+                    </div>
+                    <p>31 Jesus Saves Road,</p>
+                    <p>Asaba, Delta State</p>
+                    <p>0916 706 0239</p>
+                </div>
+                <div class="w-[250px] h-[250px] rounded-lg py-4 border-2 border-white flex justify-between items-center flex-col text-white transition-transform transition-shadow duration-300 ease-in-out hover:scale-105 hover:shadow-xl hover:border-[#CEDD71]">
+                    <div class="text-center">
+                        <p>Graceville International</p>
+                        <p>Schools</p>
+                    </div>
+                    <p>22B Summit Road,</p>
+                    <p>Asaba, Delta State</p>
+                    <p>0916 694 2397</p>
+                </div>
+                <div class="w-[250px] h-[250px] rounded-lg py-4 border-2 border-white flex justify-between items-center flex-col text-white transition-transform transition-shadow duration-300 ease-in-out hover:scale-105 hover:shadow-xl hover:border-[#CEDD71]">
+                    <div class="text-center">
+                        <p>Graceville International</p>
+                        <p>Schools</p>
+                    </div>
+                    <p>22A Summit Road,</p>
+                    <p>Asaba, Delta State</p>
+                    <p>0916 677 9461</p>
+                </div>
+                <div class="w-[250px] h-[250px] rounded-lg py-4 border-2 border-white flex justify-between items-center flex-col text-white transition-transform transition-shadow duration-300 ease-in-out hover:scale-105 hover:shadow-xl hover:border-[#CEDD71]">
+                    <div class="text-center">
+                        <p>Graceville International</p>
+                        <p>Schools</p>
+                    </div>
+                    <p class="text-center">1 Graceville Boulevard,
+                        Zoma Hills, Okpanam,</p>
+                    <p>Asaba, Delta State</p>
+                    <p>0916 702 3489</p>
+                </div>
+
+            </div>
+        </section>
+        <section>
+            <Footer />
         </section>
     </main>
 </template>
