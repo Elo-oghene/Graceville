d725addc8654b223401248ebeec66004efb07ba5
Elo-oghene Olokor <81395502+Elo-oghene@users.noreply.github.com>
2024-11-06 16:26:56 +0100

    added table content

--- src/components/auth/Studentsystem.vue
+++ src/components/auth/Studentsystem.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { ref } from 'vue';
 const attendanceOpen = ref(false); // State variable to manage the nested list visibility
+const timetableOpen = ref(false); // State variable to manage the nested list visibility
 const backgroundColor = ref('bg-white'); // Initial background color
 
 const toggleAttendance = () => {
     attendanceOpen.value = !attendanceOpen.value; // Toggle the visibility
+};
+const toggleTimetable = () => {
+    timetableOpen.value = !timetableOpen.value; // Toggle the visibility
 };
 
 const changeColor = () => {
@@ -46,52 +50,55 @@ const changeColor = () => {
 
         <div class="w-full bg-[#CEDD71] h-[2px]"></div>
         <section class="w-full h-full px-4 lg:px-16 flex flex-col py-4 gap-3 ">
-            <div class="w-full flex  justify-between">
+            <div class="w-full flex gap-2  justify-between">
                 <div class="w-full lg:w-[80%] flex flex-col gap-4 h-full">
                     <div class="w-full  flex flex-col lg:flex-row">
                         <label class="w-full lg:w-1/5 flex items-center text-sm lg:text-[16px]">STUDENTS ID</label>
-                        <div  class="w-full lg:w-4/5 h-[50px] border-2 border-[#224E05] px-4 flex items-center">fghvbjnkml,;.</div>
+                        <div  class="w-full lg:w-4/5 h-[50px] border-2 border-[#224E05] px-4 flex items-center bg-[#F5F9DA]">12345RTYZ</div>
                     </div>
                     <div class="w-full flex flex-col lg:flex-row">
                         <label class="w-full lg:w-1/5 flex items-center text-sm lg:text-[16px]">FIRSTNAME</label>
-                        <div  class="w-full lg:w-4/5 h-[50px] border-2 border-[#224E05] px-4 flex items-center">fghvbjnkml,;.</div>
+                        <div  class="w-full lg:w-4/5 h-[50px] border-2 border-[#224E05] px-4 flex items-center bg-[#F5F9DA]">Christopher</div>
                     </div>
                     <div class="w-full  flex flex-col lg:flex-row">
                         <label class="w-full lg:w-1/5 flex items-center text-sm lg:text-[16px] ">SURNAME</label>
-                        <div  class="w-full lg:w-4/5 h-[50px] border-2 border-[#224E05] px-4 flex items-center">fghvbjnkml,;.</div>
+                        <div  class="w-full lg:w-4/5 h-[50px] border-2 border-[#224E05] px-4 flex items-center bg-[#F5F9DA]">fghvbjnkml,;.</div>
                     </div>
                 </div>
-                <div class="hidden lg:flex w-[20%] border-2 border-[#CEDD71]">bxbnxn</div>
+                <div class="hidden lg:flex w-[20%] h-52 border-2 border-[#224E05]">
+                    <img src="../../assets/images/illustration.png" class="object cover w-full h-full" alt="image goes here">
+                </div>
             </div>
+
             <div class="w-full flex flex-col gap-3">
                 <div class="w-full flex flex-col lg:flex-row">
                     <label class="w-full lg:w-[16%] flex items-center text-sm lg:text-[16px]">OTHER NAMES</label>
-                    <div  class="w-full lg:w-[84%] h-[50px] border-2 border-[#224E05] px-4 flex items-center">fghvbjnkml,;.</div>
+                    <div  class="w-full lg:w-[84%] h-[50px] border-2 border-[#224E05] px-4 flex items-center bg-[#F5F9DA]">fghvbjnkml,;.</div>
                 </div>
                 <div class="w-full flex flex-col lg:flex-row">
                     <label class="w-full lg:w-[16%] flex items-center text-sm lg:text-[16px]">DATE OF BIRTH</label>
-                    <div  class="w-full lg:w-[84%] h-[50px] border-2 border-[#224E05] px-4 flex items-center">fghvbjnkml,;.</div>
+                    <div  class="w-full lg:w-[84%] h-[50px] border-2 border-[#224E05] px-4 flex items-center bg-[#F5F9DA]">fghvbjnkml,;.</div>
                 </div>
                 <div class="w-full flex flex-col lg:flex-row">
                     <label class="w-full lg:w-[16%] flex items-center text-sm lg:text-[16px]">AGE</label>
-                    <div  class="w-full lg:w-[84%] h-[50px] border-2 border-[#224E05] px-4 flex items-center">fghvbjnkml,;.</div>
+                    <div  class="w-full lg:w-[84%] h-[50px] border-2 border-[#224E05] px-4 flex items-center bg-[#F5F9DA]">fghvbjnkml,;.</div>
                 </div>
                 <div class="w-full  flex flex-col lg:flex-row">
                     <label class="w-full lg:w-[16%] flex items-center text-sm lg:text-[16px]">GENDER</label>
-                    <div  class="w-full lg:w-[84%] h-[50px] border-2 px-4 border-[#224E05]">fghvbjnkml,;.</div>
+                    <div  class="w-full lg:w-[84%] h-[50px] border-2 px-4 border-[#224E05] bg-[#F5F9DA]">fghvbjnkml,;.</div>
                 </div>
                 <div class="w-full  flex flex-col lg:flex-row">
                     <label class="w-full lg:w-[16%] flex items-center text-sm lg:text-[16px]">PARENTS PHONE NO</label>
-                    <div  class="w-full lg:w-[84%] h-[50px] border-2 border-[#224E05] px-4 flex items-center">fghvbjnkml,;.</div>
+                    <div  class="w-full lg:w-[84%] h-[50px] border-2 border-[#224E05] px-4 flex items-center bg-[#F5F9DA]">fghvbjnkml,;.</div>
                 </div>
             </div>
         </section>
         <section class="w-full px-4 lg:px-16 mt-10">
             <div>STUDENTS PROGRESS REPORT</div>
             <ul class="w-full flex flex-col mt-4 gap-4">
-                <li class="w-full h-14 bg-[#224E05] flex items-center justify-between cursor-pointer" @click="toggleAttendance">
-                    <span class="px-4 lg:px-8 text-sm lg:text-[16px]">ATTENDANCE</span>
-                    <i :class="attendanceOpen ? 'bx bx-chevron-up' : 'bx bx-chevron-down'"></i> <!-- Icon for toggling -->
+                <li class="w-full h-14 bg-[#224E05] flex items-center px-4 lg:px-8 justify-between cursor-pointer" @click="toggleAttendance">
+                    <span class="px-4 lg:px-8 text-sm lg:text-[16px] text-white">ATTENDANCE</span>
+                    <i :class="attendanceOpen ? 'bx bx-chevron-up' : 'bx bx-chevron-down'" class="text-white text-[30px]"></i> <!-- Icon for toggling -->
                 </li>
                 <div v-if="attendanceOpen" class="w-full flex gap-4 flex-col"> <!-- Nested list -->
                     <div class="flex gap-4 lg:gap-10 items-center justify-center">
@@ -110,12 +117,143 @@ const changeColor = () => {
                     </div>
                     <div class="w-full h-[30px] px-4 lg:px-8 flex items-center text-sm lg:text-lg bg-[#CEDD71]">Last present:12th May 2024</div>
                 </div>
-                <li class="w-full h-14 bg-[#224E05] flex items-center px-4 lg:px-8 text-sm lg:text-[16px]">TIME TABLE</li>
-                <li class="w-full h-14 bg-[#224E05] flex items-center px-4 lg:px-8 text-sm lg:text-[16px]">SCHOOL FEES</li>
-                <li class="w-full h-14 bg-[#224E05] flex items-center px-4 lg:px-8 text-sm lg:text-[16px]">HEALTH UPDATE</li>
-                <li class="w-full h-14 bg-[#224E05] flex items-center px-4 lg:px-8 text-sm lg:text-[16px]">TERM SCHEDULE</li>
-                <li class="w-full h-14 bg-[#224E05] flex items-center px-4 lg:px-8 text-sm lg:text-[16px]">TEST AND EXAM</li>
-                <li class="w-full h-14 bg-[#224E05] flex items-center px-4 lg:px-8 text-sm lg:text-[16px]">CHECK FINAL RESULT</li>
+                <li class="w-full h-14 bg-[#224E05] flex items-center px-4 lg:px-8 justify-between cursor-pointer" @click="toggleTimetable">
+                    <span class="px-4 lg:px-8 text-sm lg:text-[16px] text-white">TIMETABLE</span>
+                    <i :class="timetableOpen ? 'bx bx-chevron-up' : 'bx bx-chevron-down'" class="text-white text-[30px]"></i> <!-- Icon for toggling -->
+                </li>
+                <div v-if="timetableOpen" class="w-full flex gap-4 flex-col"> <!-- Nested list -->
+                <div class="overflow-x-auto">
+                    <div class="w-full border px-4 py-2 tracking-wider text-center">Primary/foundational schools Time Table 2023-24,I,II,III</div>
+                    <div class="w-full border px-4 py-2 tracking-wider ">School Name:</div>
+                    <div class="w-full border px-4 py-2 flex justify-around">
+                        <div>First Bell</div>
+                        <div>Second Bell</div>
+                        <div>School Assesment</div>
+                    </div>
+                    <table class="w-full table-auto border-collapse border border-gray-300 rounded-lg shadow-lg">
+                        <thead>                       
+                        <tr class="bg-[#f5f9da] text-center">
+                            <th class="border px-4 py-2 text-left text-xs lg:text-sm">Week</th>
+                            <th class="border px-4 py-2 text-center lg:text-left text-xs lg:text-sm">
+                                <P>1st period</P>
+                                <p>(9:15 - 9:55)</p>
+                            </th>
+                            <th class="border px-4 py-2 text-center lg:text-left text-xs lg:text-sm">
+                                <P>1st period</P>
+                                <p>(9:55 - 10:30)</p>
+                            </th>
+                            <th class="text-xs lg:text-sm"></th>
+                            <th class="border px-4 py-2 text-center lg:text-left text-xs lg:text-sm">
+                                <P>1st period</P>
+                                <p>(10:45 - 11:20)</p>
+                            </th>
+                            <th class="border px-4 py-2 text-center lg:text-left text-xs lg:text-sm">
+                                <P>1st period</P>
+                                <p>(11:20 - 11:55)</p>
+                            </th>
+                            <th class=""></th>
+                            <th class="border px-4 py-2 text-center lg:text-left text-xs lg:text-sm">
+                                <P>1st period</P>
+                                <p>(12:55 - 1:30)</p>
+                            </th>
+                            <th class="border px-4 py-2 text-center lg:text-left text-xs whitespace"></th>
+                            <th class="border px-4 py-2 ttext-center lg:text-left text-xs lg:text-sm">
+                                <P>1st period</P>
+                                <p>(1:35 - 2:00)</p>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- First Time Slot -->
+                        <tr class="text-xs lg:text-sm">
+                            <td class="border px-4 py-2">MONDAY</td>
+                            <td class="border px-4 py-2">Maths</td>
+                            <td class="border px-4 py-2">Science</td>
+                            <td class="w-14 rotate-90 mt-20"></td>
+                            <td class="border px-4 py-2">History</td>
+                            <td class="border px-4 py-2">Art</td>
+                            <td class="w-14 rotate-90"></td>
+                            <td class="border px-4 py-2">Art</td>
+                            <td class="rotate-90"></td>
+                            <td class="border px-4 py-2">Art</td>
+                        </tr>
+                        <!-- Second Time Slot -->
+                        <tr class="text-xs lg:text-sm">
+                            <td class="border px-4 py-2">TUESDAY</td>
+                            <td class="border px-4 py-2">English</td>
+                            <td class="border px-4 py-2">Maths</td>
+                            <td class=""></td>
+                            <td class="border px-4 py-2">Art</td>
+                            <td class="border px-4 py-2">History</td>
+                            <td class="w-14 rotate-90"></td>
+                            <td class="border px-4 py-2">History</td>
+                            <td class="rotate-90"></td>
+                            <td class="border px-4 py-2">History</td>
+                        </tr>
+                        <!-- Third Time Slot -->
+                        <tr class="text-xs lg:text-sm">
+                            <td class="border px-4 py-2">WEDNESDAY</td>
+                            <td class="border px-4 py-2">History</td>
+                            <td class="border px-4 py-2">English</td>
+                            <td class="rotate-90 whitespace-nowrap text-lg font-semibold">Break</td>
+                            <td class="border px-4 py-2">Science</td>
+                            <td class="border px-4 py-2">Music</td>
+                            <td class="rotate-90 whitespace-nowrap text-lg font-semibold">Break</td>
+                            <td class="border px-4 py-2">Music</td>
+                            <td class="rotate-90 whitespace-nowrap text-lg font-semibold">Break</td>
+                            <td class="border px-4 py-2">Music</td>
+                        </tr>
+                        <!-- Break Period (Place in the Break Column) -->
+                        
+                        <!-- Fourth Time Slot -->
+                        <tr class="text-xs lg:text-sm">
+                            <td class="border px-4 py-2">THURSDAY</td>
+                            <td class="border px-4 py-2">Art</td>
+                            <td class="border px-4 py-2">History</td>
+                            <td class=""></td>
+                            <td class="border px-4 py-2">Maths</td>
+                            <td class="border px-4 py-2">Science</td>
+                            <td class="rotate-90"></td>
+                            <td class="border px-4 py-2">Science</td>
+                            <td class="w-14 rotate-90"></td>
+                            <td class="border px-4 py-2">Science</td>
+                        </tr>
+                        <!-- Fifth Time Slot -->
+                        <tr class="text-xs lg:text-sm">
+                            <td class="border px-4 py-2">FRIDAY</td>
+                            <td class="border px-4 py-2">Music</td>
+                            <td class="border px-4 py-2">Art</td>
+                            <td class=""></td>
+                            <td class="border px-4 py-2">English</td>
+                            <td class="border px-4 py-2">Maths</td>
+                            <td class="w-14 rotate-90"></td>
+                            <td class="border px-4 py-2">Maths</td>
+                            <td class=""></td>
+                            <td class="border px-4 py-2">Maths</td>
+                        </tr>
+                        <!-- Fifth Time Slot -->
+                        <tr class="text-xs lg:text-sm">
+                            <td class="border px-4 py-2">SATURDAY</td>
+                            <td class="border px-4 py-2">Music</td>
+                            <td class="border px-4 py-2">Art</td>
+                            <td class=""></td>
+                            <td class="border px-4 py-2">English</td>
+                            <td class="border px-4 py-2">Maths</td>
+                            <td class="w-14 rotate-90"></td>
+                            <td class="border px-4 py-2">Maths</td>
+                            <td class="w-14 rotate-90"></td>
+                            <td class="border px-4 py-2">Maths</td>
+                        </tr>
+                       
+                    </tbody>
+                    </table>
+                </div>
+                </div>
+                <li class="w-full h-14 bg-[#224E05] flex items-center px-4 lg:px-8 text-sm lg:text-[16px] text-white">SCHOOL FEES</li>
+                <li class="w-full h-14 bg-[#224E05] flex items-center px-4 lg:px-8 text-sm lg:text-[16px] text-white">HEALTH UPDATE</li>
+                <li class="w-full h-14 bg-[#224E05] flex items-center px-4 lg:px-8 text-sm lg:text-[16px] text-white">TERM SCHEDULE</li>
+                <li class="w-full h-14 bg-[#224E05] flex items-center px-4 lg:px-8 text-sm lg:text-[16px] text-white">TEST AND EXAM</li>
+                <li class="w-full h-14 bg-[#224E05] flex items-center px-4 lg:px-8 text-sm lg:text-[16px] text-white">CHECK FINAL RESULT</li>
             </ul>
         </section>
     </section>
